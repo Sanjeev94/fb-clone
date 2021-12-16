038b6b9d3cf00bce76212c6c923fac00886e5ebc
Sanjeev94 <sanjeev.dubey930@gmail.com>
2021-12-17 05:06:23 +0530

    Widgits added
    next-> task login

--- src/App.js
+++ src/App.js
@@ -3,18 +3,28 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import Widgets from './Widgets';
 
 function App() {
+  const user = "Sanjeev";
   return (
     <div className="app">
+      {!user ? (
+        <h1>Login</h1>
+      ) : (
+        <>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          <Widgets />
+  
+        </div>
+        </>
+      )}
       {/* <h1>Lets build the facebook clone!</h1> */}
 
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-
-      </div>
+      
       {/* app body */}
       {/* Header */}
       {/* Sidebar */}
